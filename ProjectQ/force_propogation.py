1d6274f9371c5f8bb6a3d3b3fc6c4bbde7a5d54b
Jason Kang <jason.kang@vpn-10-1-178-78.vpnclient.harvard.edu>
2019-05-14 14:23:34 -0400

    first attempt at force propogation

--- ProjectQ/force_propogation.py
+++ ProjectQ/force_propogation.py
@@ -1,36 +1,29 @@
-import numpy
-import scipy
-import scipy.linalg
-
+import os
 from numpy import array, concatenate, zeros
 from numpy.random import randn
 from scipy.optimize import minimize
-
 from openfermion.config import *
 from openfermionprojectq import *
-
 from openfermion.hamiltonians import MolecularData
 from openfermion.transforms import jordan_wigner, get_fermion_operator, get_sparse_operator
 from openfermion.utils import uccsd_singlet_paramsize
-
 from projectq.ops import X, All, Measure
-from projectq.backends import CommandPrinter, CircuitDrawer
+from projectq.backends import CommandPrinter, CircuitDrawer, IBMBackend
+from pyscf import mp, fci
 
-import matplotlib.pyplot as plt
 
 from openfermionpyscf import run_pyscf
+
 
 def energy_objective(packed_amplitudes):
     """Evaluate the energy of a UCCSD singlet wavefunction with packed_amplitudes
     Args:
         packed_amplitudes(ndarray): Compact array that stores the unique
             amplitudes for a UCCSD singlet wavefunction.
-
     Returns:
         energy(float): Energy corresponding to the given amplitudes
     """
     os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
     # Set Jordan-Wigner initial state with correct number of electrons
     wavefunction = compiler_engine.allocate_qureg(molecule.n_qubits)
     for i in range(molecule.n_electrons):
@@ -42,7 +35,6 @@ def energy_objective(packed_amplitudes):
                                                  molecule.n_electrons)
     evolution_operator | wavefunction
     compiler_engine.flush()
-
     # Evaluate the energy and reset wavefunction
     energy = compiler_engine.backend.get_expectation_value(qubit_hamiltonian, wavefunction)
     All(Measure) | wavefunction
@@ -50,19 +42,9 @@ def energy_objective(packed_amplitudes):
     return energy
 
 
-# Load saved file for H2 + H.
+# Load saved file for H3.
 basis = 'sto-3g'
 spin = 2
-n_points = 40
-bond_length_interval = 3.0 / n_points
-bond_lengths = []
-
-fci_energies = []
-UCCSD_energies = []
-
-# Set Hamiltonian parameters.
-active_space_start = 1
-active_space_stop = 3
 
 # Set calculation parameters.
 run_scf = 1
@@ -73,17 +55,42 @@ run_fci = 1
 delete_input = True
 delete_output = True
 
+# Set Hamiltonian parameters.
+active_space_start = 1
+active_space_stop = 3
 
-f = open('ProjectQ-h3-results.txt', 'w')
+# Set record list for plotting. Existing information input from ProjectQ simulator.
+bond_lengths = [0.73]
+fci_energies = [-1.603565128035238, -1.6004199263636436]
+UCCSD_energies = [-1.5836999664044602, -1.5771459927119653]
 
-for point in range(1, n_points + 1):
-    bond_length = bond_length_interval * float(point) + 0.2
-    bond_lengths += [bond_length]
-    geometry = [('H', (0., 0., 0.)), ('H', (0., 0., bond_length)), ('H', (0., 0., 3.3))]
+#initial force as calculated by fci
+force_list = [0.041936022287925389248029717233852330706289645136440941719]
+
+distance_counter = 0.8
+
+initial_velocity = 1.10305*10**(-30)
+
+mass = 1.6735575*10**(-27)
+
+time = 1
+
+while distance_counter < 3.0
+    bond_lengths += [distance_counter]
+
+    force = force_list[-1]
+
+    acceleration = (force/mass * 4.359744650*(10**(-28)) * ((10**10)**2) *
+        (2.41888*10**(-17))**2)
+
+    distance_counter += acceleration*1/2*time**2 + initial_velocity*time
+
+
+    geometry = [('H', (0., 0., 0.)), ('H', (0., 0., distance_counter)),
+                ('H', (0., 0., 3.3))]
 
     # Generate and populate instance of MolecularData.
-    molecule = MolecularData(geometry, basis, spin, description=str(round(bond_length, 2)))
-
+    molecule = MolecularData(geometry, basis, spin, description="h3")
 
     molecule = run_pyscf(molecule,
                          run_scf=run_scf,
@@ -92,18 +99,14 @@ for point in range(1, n_points + 1):
                          run_ccsd=run_ccsd,
                          run_fci=run_fci)
 
-    # Get the Hamiltonian in an active space.
+    # Use a Jordan-Wigner encoding, and compress to remove 0 imaginary components
     molecular_hamiltonian = molecule.get_molecular_hamiltonian(
         occupied_indices=range(active_space_start),
         active_indices=range(active_space_start, active_space_stop))
 
-    # Map operator to fermions and qubits.
     fermion_hamiltonian = get_fermion_operator(molecular_hamiltonian)
     qubit_hamiltonian = jordan_wigner(fermion_hamiltonian)
-
-    # compress removes 0 entries. qubit_hamiltonian is a qubit_operator
     qubit_hamiltonian.compress()
-
     compiler_engine = uccsd_trotter_engine()
 
     n_amplitudes = uccsd_singlet_paramsize(molecule.n_qubits, molecule.n_electrons)
@@ -117,67 +120,31 @@ for point in range(1, n_points + 1):
     opt_energy, opt_amplitudes = opt_result.fun, opt_result.x
 
     fci_energies += [float(molecule.fci_energy)]
-    UCCSD_energies += [float(opt_energy)]
+    # UCCSD_energies += [float(opt_energy)]
 
-    # write results into txt file
-    f = open('ProjectQ-h3-results.txt', 'a')
-    f.write("Results for {}: \n".format(molecule.name))
-    f.write("Bond Length: {} \n".format(str(bond_length)))
-    f.write("Optimal UCCSD Singlet Energy: {} \n".format(str(opt_energy)))
-    f.write("Optimal UCCSD Singlet Amplitudes: ")
-    for item in opt_amplitudes:
-        f.write("{} ".format(str(item)))
-    f.write("\n")
+    force_list += [(fci_energies[-1] - fci_energies[-2])/(distance_counter - bond_lengths[-1])]
 
-    f.write("Classical CCSD Energy: {} Hartrees \n".format(str(float(item))))
-    f.write("Exact FCI Energy: {} Hartrees \n".format(str(float(molecule.fci_energy))))
-    f.write("Initial Energy of UCCSD with CCSD amplitudes: {} Hartrees \n\n".format(str(float(initial_energy))))
-
-    f.close
-
-    print("\nResults for {}:".format(molecule.name))
-    print("Bond Length: {} \n".format(str(bond_length)))
+    print("\n Results for {}:".format(molecule.name))
     print("Optimal UCCSD Singlet Energy: {}".format(opt_energy))
     print("Optimal UCCSD Singlet Amplitudes: {}".format(opt_amplitudes))
     print("Classical CCSD Energy: {} Hartrees".format(molecule.ccsd_energy))
     print("Exact FCI Energy: {} Hartrees".format(molecule.fci_energy))
-    print("Initial Energy of UCCSD with CCSD amplitudes: {} Hartrees\n\n".format(initial_energy))
+    print("Initial Energy of UCCSD with CCSD amplitudes: {} Hartrees".format(initial_energy))
 
 
+print("This is bond lengths:", bond_lengths)
 
-# plot energies
+print("This is force lists:", force_list)
+
+adjusted_lengths = [1/2(b - a) for a, b in zip(bond_lengths, bond_lengths[1:])]
+
+print("This is adjusted lengths:", adjusted_lengths)
+
 f1 = plt.figure(0)
-plt.plot(bond_lengths, fci_energies, 'x-')
-plt.plot(bond_lengths, UCCSD_energies, 'o-')
-plt.ylabel('Energy in Hartree')
+plt.plot(adjusted_lengths, force_list, 'x-')
+plt.ylabel('Force in Hartree/angstrom')
 plt.xlabel('Bond length in angstrom')
 
-plt.savefig("ProjectQ-h3-graph", dpi=400, orientation='portrait')
-
-plt.show()
-
-
-
-
-
-f2 = plt.figure(1)
-energy_delta = [b - a for a, b in zip(fci_energies, fci_energies[1:])]
-print ("This is the energy_delta:", energy_delta)
-
-length_delta = [b - a for a, b in zip(bond_lengths, bond_lengths[1:])]
-print ("This is the length_delta:", length_delta)
-
-force = [a/b for a, b in zip(energy_delta, length_delta)]
-print ("This is the force:", force)
-
-lengths = [1/2(b - a) for a, b in zip(bond_lengths, bond_lengths[1:])]
-print ("This is the lengths:",lengths)
-
-f2 = plt.figure(0)
-plt.plot(lengths, force, 'x-')
-plt.ylabel('Energy in Hartree / angstrom')
-plt.xlabel('Bond length in angstrom')
-
-plt.savefig("ProjectQ-h3-force-graph", dpi=400, orientation='portrait')
+plt.savefig("Force-Propogation-h3-graph", dpi=400, orientation='portrait')
 
 plt.show()
