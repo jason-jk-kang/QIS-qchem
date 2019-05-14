cf2f8113b65510ee1efd0ae0070b071ad69a9728
Jason Kang <jason.kang@vpn-10-1-178-78.vpnclient.harvard.edu>
2019-05-14 15:45:11 -0400

    minor edits

--- ProjectQ/test.py
+++
@@ -1,9 +0,0 @@
-force_list = []
-fci_energies = [-1.603565128035238, -1.6004199263636436]
-distance_counter = 0.8
-bond_lengths = [0.725]
-
-
-force_list += [(fci_energies[-1] - fci_energies[-2])/(distance_counter - bond_lengths[-1])]
-
-print(force_list)
